3536e6142c0215420768d380ac3611ba0ac3f0d9
Eva Afriyanti <evasaja123456@gmail.com>
2020-11-23 00:25:17 +0700

    tugasatw6

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Eva Afriyanti
  */
 
 define('LARAVEL_START', microtime(true));
